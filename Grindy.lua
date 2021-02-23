2b9f62f220ddc3827ad13d52f5b62a87332bb77d
Ben Brookes <brookesb91@hotmail.com>
2021-02-23 09:25:56 +0000

    :wrench: Refactor & clean-up

--- Grindy.lua
+++ Grindy.lua
@@ -1,105 +1,123 @@
 local Grindy = CreateFrame("Frame");
-local Player, Cache, Last, Rate;
 
-Grindy:RegisterEvent("PLAYER_LOGIN")
-Grindy:RegisterEvent("CHAT_MSG_COMBAT_XP_GAIN")
-Grindy:SetScript("OnEvent",
-  function(self, event, ...)
-    self:SetPlayerXp()
-    if event == "CHAT_MSG_COMBAT_XP_GAIN" then
-      self:OnXpGain(...)
-    end
-  end
-)
+local Player, Cache, Rate, Info, Colors;
 
-function Grindy:OnXpGain(...)
-  local text = ...
-  local gained = tonumber(string.match(text, "(%d+)"))
-  local enemy = string.match(text, "(.+).dies")
-  local formatted = enemy:lower():gsub("%s", "_")
+Player = { StartTime = GetTime() }
+Info = {}
+Cache = {}
+Rate = { Change = 0, Current = 0 }
+Colors = {
+  Green = "FF10C15C",
+  Red = "FFD05353",
+  Blue = "FF227C9D",
+  Yellow = "FFFFCB77"
+}
 
-  Player.Latest = gained;
-  Player.Session = Player.Session + gained;
-
-  if (gained > 0) then
-    local toLevel = ceil((Player.Max - Player.Current) / gained)
-    self:AddMessage(
-      self:GetDiffString(gained, Player.Latest).." |C0000FFFF"
-      ..toLevel.." |C007FBA32more |C00FFFFFF"..enemy.." |C007FBA32to level."
-    )
-  end
-
-  if Cache[formatted] then
-    Cache[formatted] = {
-      Yield = gained,
-      Count = Cache[formatted].Count + 1,
-      Time = GetTime()
-    }
-  else
-    Cache[formatted] = {
-      Yield = gained,
-      Count = 1,
-      Time = GetTime()
-    }
-  end
-
-  if Last then
-    local current = Cache[formatted]
-    local rate = ceil(current.Yield / ((current.Time - Last.Time) / 60 / 60))
-    Grindy:AddMessage(
-      "|C00FFFFFF[Rate] "
-      ..self:GetDiffString(rate, Rate)..
-      " |C0000FFFF"..string.gsub(rate, "^(-?%d+)(%d%d%d)", '%1,%2')..
-      "|C00FFFFFF XP/h"
-    )
-
-    Rate = rate;
-  end
-
-  Last = Cache[formatted]
+function Cache:Init()
+  self = {}
 end
 
-function Grindy:SetPlayerXp()
-  Player.Current = UnitXP("player")
-  Player.Max = UnitXPMax("player")
+function Cache:Add(key, time)
+  local total = self[key] ~= nil and self[key].Count + 1 or 1
+
+  self[key] = {
+    Count = total,
+    Time = time
+  }
 end
 
-function Grindy:GetDiffString(amount, compare)
-  local diff;
-  if amount > compare then
-    diff = "|C0000FF00[+]"
-  elseif amount < compare then
-    diff = "|C00FF0000[-]"
-  else
-    diff = "|C00FF00FF[~]"
-  end
-  return diff;
+function Info:From(message)
+  local PATTERN_XP_GAINED = "(%d+)"
+  local PATTERN_UNIT_NAME = "(.+).dies"
+
+  local amount = tonumber(string.match(message, PATTERN_XP_GAINED))
+  local name = string.match(message, PATTERN_UNIT_NAME)
+  local key = name:lower():gsub("%s", "_")
+
+  return name, key, amount
 end
 
+function Rate:Init()
+  self.Change = 0
+  self.Current = 0
+end
 
-function SlashHandler(arg1)
-  if arg1:lower():find("reset") then
-    Grindy:Init()
-    Grindy:AddMessage("|C007FBA32Successfully reset.")
-  else
-    Grindy:AddMessage("|C00FFFFFFOption not found.")
+function Rate:Update(rate)
+  self.Change = (rate - self.Current)
+  self.Current = rate
+end
+
+function Player:Init()
+  self.StartTime = GetTime()
+end
+
+function Player:Update(amount)
+  self.Current = UnitXP("player")
+  self.Max = UnitXPMax("player")
+  self.Latest = amount
+  self.Total = self.Total ~= nil and self.Total + amount or amount
+  self.ToLevel = self.Max - self.Current
+end
+
+local function format_number(amount)
+  return string.gsub(amount, "^(-?%d+)(%d%d%d)", '%1,%2')
+end
+
+function Grindy:Handle(...)
+  local message = ...
+  local name, key, amount = Info:From(message)
+
+  if amount > 0 then
+    Player:Update(amount)
+
+    local time = GetTime()
+    local rate = ceil(Player.Total / ((time - Player.StartTime) / 60 / 60))
+    local to_level = ceil(Player.ToLevel / amount)
+
+    Rate:Update(rate)
+
+    local to_level_str = format_number(to_level)
+    local rate_current_str = format_number(Rate.Current)
+    local rate_change_str = format_number(Rate.Change)
+    local change_color = Rate.Change == 0 and Colors.Blue or
+      (Rate.Change < 0 and Colors.Red or Colors.Blue)
+
+    Cache:Add(key, time)
+    self:AddMessage("|C"..Colors.Blue..""..to_level_str.." more "..name.." to level.")
+    self:AddMessage("|C"..Colors.Yellow.."Current Rate: "..rate_current_str.." xp/h |C"..change_color.."("..rate_change_str..")")
   end
 end
 
 function Grindy:AddMessage(message)
-  local intro = "|C00C0FFEEGrindy:";
-  DEFAULT_CHAT_FRAME:AddMessage(""..intro.." "..message.."")
+  DEFAULT_CHAT_FRAME:AddMessage("[Grindy] "..message.."")
 end
 
 function Grindy:Init()
-  Cache = {}
-  Rate = 0
-  Player = {Current = 0, Max = 0, Latest = 0, Session = 0}
-  Last =  {Yield = 0, Time = GetTime()}
+  Cache:Init()
+  Player:Init()
+  Rate:Init()
+  self:AddMessage("Initialised!")
+end
+
+function SlashHandler(arg1)
+  if arg1:lower():find("reset") then
+    Grindy:Init()
+  else
+    Grindy:AddMessage("Option not found.")
+  end
 end
 
 SlashCmdList["GrindyCommand"] = SlashHandler;
 SLASH_GrindyCommand1 = "/grindy"
 
+Grindy:RegisterEvent("PLAYER_LOGIN")
+Grindy:RegisterEvent("CHAT_MSG_COMBAT_XP_GAIN")
+Grindy:SetScript("OnEvent",
+  function(self, event, ...)
+    if event == "CHAT_MSG_COMBAT_XP_GAIN" then
+      self:Handle(...)
+    end
+  end
+)
+
 Grindy:Init()
-Grindy:AddMessage("|CFF7FBA32Loaded!")
